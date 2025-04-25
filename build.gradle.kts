829800ff845034069d3c3cea762cd7c5bc136b9c
Asmaa7071 <100071554+Asmaa7071@users.noreply.github.com>
2025-04-25 15:51:26 +0300

    add unit test for search meal by name (#69)
    * add maxError parameter in invoke function
    
    * add unit test for SearchMealByNameUseCaseTest
    
    ---------
    
    Co-authored-by: Asmaa Karam <asmaakaram793@gmai.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("io.insert-koin:koin-core:4.0.2")
 
 
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation(kotlin("test"))
 
@@ -23,7 +24,6 @@ dependencies {
 
     testImplementation("com.google.truth:truth:1.4.4")
     testImplementation("io.mockk:mockk:1.14.0")
-
 
 }
 
