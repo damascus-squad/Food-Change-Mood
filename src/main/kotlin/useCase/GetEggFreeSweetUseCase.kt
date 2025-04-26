459c67b773c2ecaa8d3b62010b3f8eb29d3c1fc3
Yusuf Nasser <yusufnassereng@gmail.com>
2025-04-26 15:38:45 +0300

    refactor meal repository interface
    - Move repository to logic pkg
    - Move use-case functions out of the repository & place them in their respective use-case classes.

--- src/main/kotlin/useCase/GetEggFreeSweetUseCase.kt
+++ src/main/kotlin/useCase/GetEggFreeSweetUseCase.kt
@@ -6,5 +6,20 @@ import org.damascus.model.Meal
 class GetEggFreeSweetUseCase(
     private val mealRepo: MealRepository
 ) {
-    operator fun invoke(): Meal = mealRepo.getEggFreeSweet()
+    private val suggestedEggFreeSweets = mutableSetOf<Int>()
+
+    operator fun invoke(): Meal  {
+        val allMeals = mealRepo.getAllMeals()
+        val eggFree = allMeals.filter { meal ->
+            meal.tags.any { it.contains("sweet", true) } &&
+                    !meal.ingredients.any { it.contains("egg", true) } &&
+                    !suggestedEggFreeSweets.contains(meal.id)
+        }
+
+        if (eggFree.isEmpty()) throw NoSuchElementException("No more egg-free sweets left.")
+
+        val next = eggFree.random()
+        suggestedEggFreeSweets.add(next.id)
+        return next
+    }
 }
--- src/main/kotlin/useCase/GetHighCalorieMealUseCase.kt
+++ src/main/kotlin/useCase/GetHighCalorieMealUseCase.kt
@@ -4,7 +4,22 @@ import org.damascus.logic.MealRepository
 import org.damascus.model.Meal
 
 class GetHighCalorieMealUseCase(
-    private val repo: MealRepository
+    private val mealRepo: MealRepository
 ) {
-    operator fun invoke(): List<Meal> = repo.getHighCalorieMeal()
+    private val suggestedHighCalorieMeals = mutableSetOf<Int>()
+
+    operator fun invoke(): List<Meal> {
+        val allMeals = mealRepo.getAllMeals()
+
+        val highCalMeals = allMeals.filter {
+            it.nutrition.calories > 700 && !suggestedHighCalorieMeals.contains(it.id)
+        }
+
+        if (highCalMeals.isEmpty()) throw NoSuchElementException("No more high-calorie meals.")
+
+        val selected = highCalMeals.random()
+        suggestedHighCalorieMeals.add(selected.id)
+
+        return highCalMeals
+    }
 }
